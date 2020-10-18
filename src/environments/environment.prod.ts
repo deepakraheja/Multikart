8a1dc4c0a502aa5004f84821a00fd8659de6cd55
Kit <deepakrahejain@gmail.com>
2020-10-18 21:06:08 +0530

    commit

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -6,9 +6,8 @@ export const environment = {
 
   //*********************Production Server************* */
 
-
-  BASE_API_URL: 'http://34.67.65.213/EcommApiV3/api/',
-  ProductImage: 'http://34.67.65.213/EcommApiV3/ProductImage/',
-  Report_Path: 'http://34.67.65.213/EcommApiV3/ReportGenerate/',
-  WebSite_URL: 'http://34.67.65.213/Ecom/'
+  BASE_API_URL: 'http://34.72.151.153/EcommApiV3/api/',
+  ProductImage: 'http://34.72.151.153/EcommApiV3/ProductImage/',
+  Report_Path: 'http://34.72.151.153/EcommApiV3/ReportGenerate/',
+  WebSite_URL: 'http://34.72.151.153/Ecom/'
 };
